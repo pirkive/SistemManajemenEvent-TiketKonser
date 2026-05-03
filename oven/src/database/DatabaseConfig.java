feddbb757d5ef9db73c670e44f24e70aa8543aaa
pirkive <kaniaputrimahpira@gmail.com>
2026-05-03 16:02:36 +0700

    Menambahkan Multi-Host Connection pada File DatabseConfig

--- oven/src/database/DatabaseConfig.java
+++ oven/src/database/DatabaseConfig.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:mysql://localhost:3307/eticket_db";
+    private static final String URL = "jdbc:mysql://localhost:3306,localhost:3307/eticket_db?connectTimeout=5000";
     private static final String USER = "root";
     private static final String PASS = "";
 
